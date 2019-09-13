3261ac852573adc2d7261f610c52afd54366df14
MrSaighnal <47419260+MrSaighnal@users.noreply.github.com>
2019-09-13 20:28:47 +0200

    Update screen-resolution-forcer.sh

--- screen-resolution-forcer.sh
+++ screen-resolution-forcer.sh
@@ -1,4 +1,5 @@
 xrandr --newmode "1920x1050"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode Virtual1 1920x1050
 xrandr --output Virtual1 --mode 1920x1050
+echo "new resolution 1920x1080"
 
