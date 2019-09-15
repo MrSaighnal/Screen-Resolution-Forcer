c18915b5290d940ccbb24cb3c0acbe9479954e27
MrSaighnal <47419260+MrSaighnal@users.noreply.github.com>
2019-09-15 12:55:10 +0200

    Update screen-resolution-forcer.sh

--- screen-resolution-forcer.sh
+++ screen-resolution-forcer.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+#Name:			Screen-Resolution-Forcer
+#Author:		MrSaighnal
+#date:			14/09/19
+#officiale Repository:	https://github.com/MrSaighnal/Screen-Resolution-Forcer
+
 xrandr --newmode "1920x1050"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode Virtual1 1920x1050
 xrandr --output Virtual1 --mode 1920x1050
